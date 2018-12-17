90370fc4cb784da2898a9bd1fc972a9bccd0e5ad
changje cho <qkdrk777777@naver.com>
2018-12-17 12:57:21 +0900

    commit

--- R/town_new_api.R
+++ R/town_new_api.R
@@ -5,12 +5,13 @@
 #'  @param category= POP,PTY, REH,SKY,T3H,UUU,VEC,VVV,WSD,WAV
 #'  @param url = api url
 #'  @return
-#'  @examples pack2(c('xml2','XML','plyr'))
-#'  key='XqCpVL8gGGZXQquN9iXsLtBPS6dwAgnxfWorz1IlKu77R72UPgvE7Lx1J1%2BITprn3%2BoZ0cU3IQ1JnRCpEJ%2BkZw%3D%3D'
-#'  url=paste0('http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=',
-#'  key,paste0('&base_date=',gsub('-','',substr(Sys.time()-3600*c(23:0),1,10))[substr(Sys.time()-3600*c(23:0),12,13)%in%c('02','05','08','11','14','17','20','23')]
-#'  ,'&base_time=',paste0(substr(Sys.time()-3600*c(23:0),12,13)[substr(Sys.time()-3600*c(23:0),12,13)%in%c('02','05','08','11','14','17','20','23')],'00')),paste0('&nx=',unique(area[,1:2])[,1],'&ny=',unique(area[,1:2])[,2]))
-#'   api_forecast(cat='T3H',url[1:2])
+#'  @example
+ #  pack2(c('xml2','XML','plyr'))
+ # key='XqCpVL8gGGZXQquN9iXsLtBPS6dwAgnxfWorz1IlKu77R72UPgvE7Lx1J1%2BITprn3%2BoZ0cU3IQ1JnRCpEJ%2BkZw%3D%3D'
+ # url=paste0('http://newsky2.kma.go.kr/service/SecndSrtpdFrcstInfoService2/ForecastSpaceData?ServiceKey=',
+ # key,paste0('&base_date=',gsub('-','',substr(Sys.time()-3600*c(23:0),1,10))[substr(Sys.time()-3600*c(23:0),12,13)%in%c('02','05','08','11','14','17','20','23')]
+ # ,'&base_time=',paste0(substr(Sys.time()-3600*c(23:0),12,13)[substr(Sys.time()-3600*c(23:0),12,13)%in%c('02','05','08','11','14','17','20','23')],'00')),paste0('&nx=',unique(area[,1:2])[,1],'&ny=',unique(area[,1:2])[,2]))
+ #  api_forecast(cat='T3H',url[1:2])
 #'  @export
 api_forecast=function(cat='T3H',url=url[1:2]){
 data2=lapply(url,function(x){xmlToDataFrame(nodes=getNodeSet(xmlParse(x),'//response/body/items/item'),stringsAsFactors = F)})
